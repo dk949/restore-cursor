f0c5d6cf1d96bdbd2d4b2a6c373b4923e70ff5a4
dk949 <56653556+dk949@users.noreply.github.com>
2025-01-25 02:32:42 +0000

    some minor fixes to how options are handled

--- lua/remember-where/init.lua
+++ lua/remember-where/init.lua
@@ -55,7 +55,7 @@ local default_patterns = {
 }
 
 ---@type Options
-local default_optiosn = {
+local default_options = {
     pat = default_patterns,
     event = "BufReadPost",
     cb_pre = nil,
@@ -114,29 +114,36 @@ local function shouldRun()
 end
 
 
----@param opt Options
-return function(opt)
-    if not opt then opt = {} end
-    ---@type Options
-    opt = vim.tbl_deep_extend("keep", opt, default_optiosn)
-    if opt.pat.only then opt.pat.override_ignore = nil end
-    opt.pat = vim.tbl_deep_extend("keep", typeCheckPattern(opt.pat), default_patterns)
+---@param opts Options
+---@return Options
+local function fillDefaultOpts(opts)
+    if not opts then return default_options end
+    opts.pat = vim.tbl_deep_extend("keep", typeCheckPattern(opts.pat), default_patterns)
+    if opts.event == nil then opts.event = default_options.event end
+    return opts
+end
+
+
+---@param opts Options
+return function(opts)
+    opts = fillDefaultOpts(opts);
+    if opts.pat.only then opts.pat.override_ignore = nil end
 
     local M = {}
 
     local function remember(ev)
         if not shouldRun() then return end
-        if not bufMatches(ev.file, vim.bo.filetype, opt.pat) then return end
+        if not bufMatches(ev.file, vim.bo.filetype, opts.pat) then return end
         local line = vim.fn.line("'\"")
         if line < 1 or line > vim.fn.line("$") then return end
-        if opt.cb_pre then opt.cb_pre() end
-        vim.api.nvim_feedkeys('g`"', "n", false)
-        if opt.cb_post then opt.cb_post() end
+        if opts.cb_pre then opts.cb_pre() end
+        vim.cmd[[keepjumps norm g`"]]
+        if opts.cb_post then opts.cb_post() end
     end
 
     function M.installHandler()
         M.group = vim.api.nvim_create_augroup("remember_where_group", { clear = true })
-        M.autocmd = vim.api.nvim_create_autocmd(opt.event, {
+        M.autocmd = vim.api.nvim_create_autocmd(opts.event, {
             group = M.group,
             callback = remember,
             desc = [[When file is opened, jump to the last position of the cursor]],
